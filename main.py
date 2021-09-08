4889a38f0e8c762d9f434c57521085926d0e3315
Damon Holland <damonrholland@gmail.com>
2021-09-08 11:48:35 -0700

    Swapped rendering method to PyOpenGL

--- main.py
+++ main.py
@@ -1,81 +1,76 @@
 import pygame
+from pygame.locals import *
 import numpy as np
 from math import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 WINDOW_HEIGHT = 720
 WINDOW_WIDTH = 720
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
-ORANGE = (255, 128, 0)
-COLORS = [RED, GREEN, BLUE, WHITE, YELLOW, ORANGE]
-SCALE = 100
+RED = (1, 0, 0)
+GREEN = (0, 1, 0)
+BLUE = (0, 0, 1)
+WHITE = (1, 1, 1)
+YELLOW = (1, 1, 0)
+ORANGE = (1, 0.5, 0)
+COLORS = [RED, ORANGE, YELLOW, WHITE, BLUE, GREEN]
 ROTATE_SPEED = 0.005
 x_angle = 90
 y_angle = 90
 z_angle = 0
-drag = False
 
 def draw_cube(cube_x, cube_y, cube_z):
     width = 0.5
-    points = [np.matrix([cube_x + width, cube_y + width, cube_z + width]),
-              np.matrix([cube_x + width, cube_y + width, cube_z - width]),
-              np.matrix([cube_x + width, cube_y - width, cube_z + width]),
-              np.matrix([cube_x + width, cube_y - width, cube_z - width]),
-              np.matrix([cube_x - width, cube_y + width, cube_z + width]),
-              np.matrix([cube_x - width, cube_y + width, cube_z - width]),
-              np.matrix([cube_x - width, cube_y - width, cube_z + width]),
-              np.matrix([cube_x - width, cube_y - width, cube_z - width])]
+    vertices = (
+        np.matrix([cube_x - width, cube_y - width, cube_z - width]),
+        np.matrix([cube_x - width, cube_y + width, cube_z - width]),
+        np.matrix([cube_x - width, cube_y + width, cube_z + width]),
+        np.matrix([cube_x - width, cube_y - width, cube_z + width]),
+        np.matrix([cube_x + width, cube_y - width, cube_z - width]),
+        np.matrix([cube_x + width, cube_y + width, cube_z - width]),
+        np.matrix([cube_x + width, cube_y + width, cube_z + width]),
+        np.matrix([cube_x + width, cube_y - width, cube_z + width])
+    )
+    edges = ((0, 1),(0, 3),(0, 4),(1, 2),(1, 5),(2, 3),(2, 6),(3, 7),(4, 5),(4, 7),(5, 6),(6, 7))
+    faces = ((0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 7, 3), (1, 5, 6, 2), (2, 6, 7, 3), (1, 5, 4, 0))
+
     rotation_x = np.matrix([[1, 0, 0], [0, cos(x_angle), -sin(x_angle)], [0, sin(x_angle), cos(x_angle)]])
     rotation_y = np.matrix([[cos(y_angle), 0, sin(y_angle)], [0, 1, 0], [-sin(y_angle), 0, cos(y_angle)]])
-    rotation_z = np.matrix([[cos(z_angle), -sin(z_angle), 0], [sin(z_angle), cos(z_angle), 0],[0, 0, 1]])
+    rotation_z = np.matrix([[cos(z_angle), -sin(z_angle), 0], [sin(z_angle), cos(z_angle), 0], [0, 0, 1]])
 
-    projected_points = []
-    for point in points:
-        rotated2d = np.dot(rotation_z, point.reshape((3, 1)))
-        rotated2d = np.dot(rotation_y, rotated2d)
-        rotated2d = np.dot(rotation_x, rotated2d)
-        projected2d = np.dot(np.matrix([[1, 0, 0], [0, 1, 0]]), rotated2d)
-        x = int(projected2d[0][0] * SCALE) + WINDOW_WIDTH / 2
-        y = int(projected2d[1][0] * SCALE) + WINDOW_HEIGHT / 2
-        projected_points.append([x, y])
+    rotated_points = []
+    for vertex in vertices:
+        rotatedVertex = np.dot(rotation_z, vertex.reshape((3, 1)))
+        rotatedVertex = np.dot(rotation_y, rotatedVertex)
+        rotatedVertex = np.dot(rotation_x, rotatedVertex)
+        rotated_points.append(rotatedVertex)
 
-    projected_edges = [ [projected_points[0], projected_points[1]],
-                        [projected_points[0], projected_points[2]],
-                        [projected_points[0], projected_points[4]],
-                        [projected_points[1], projected_points[3]],
-                        [projected_points[1], projected_points[5]],
-                        [projected_points[2], projected_points[3]],
-                        [projected_points[2], projected_points[6]],
-                        [projected_points[3], projected_points[7]],
-                        [projected_points[4], projected_points[5]],
-                        [projected_points[4], projected_points[6]],
-                        [projected_points[5], projected_points[7]],
-                        [projected_points[6], projected_points[7]] ]
-
-    for edge in projected_edges:
-        pygame.draw.line(screen, WHITE, edge[0], edge[1])
-
-    projected_faces = [ [projected_edges[0][0], projected_edges[5][0], projected_edges[5][1], projected_edges[0][1]],
-                        [projected_edges[0][0], projected_edges[8][0], projected_edges[8][1], projected_edges[0][1]],
-                        [projected_edges[1][0], projected_edges[9][0], projected_edges[9][1], projected_edges[1][1]],
-                        [projected_edges[3][0], projected_edges[10][0], projected_edges[10][1], projected_edges[3][1]],
-                        [projected_edges[5][0], projected_edges[11][0], projected_edges[11][1], projected_edges[5][1]],
-                        [projected_edges[8][0], projected_edges[11][0], projected_edges[11][1], projected_edges[8][1]]]
-
+    glBegin(GL_QUADS)
     i = 0
-    for face in projected_faces:
-        pygame.draw.polygon(screen, COLORS[i], [face[0], face[1], face[2], face[3]])
+    for face in faces:
+        for vertex in face:
+            glColor3fv(COLORS[i])
+            glVertex3fv(rotated_points[vertex])
         i += 1
+    glEnd()
+
+    glBegin(GL_LINES)
+    glColor3fv(BLACK)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(rotated_points[vertex])
+    glEnd()
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF|OPENGL)
+    gluPerspective(45, WINDOW_WIDTH/WINDOW_HEIGHT, 0.1, 50.0)
+    glEnable(GL_DEPTH_TEST)
+    glTranslatef(0.0, 0.0, -8)
     pygame.display.set_caption("Rubik's Cube")
     running = True
+    drag = False
     prev_mouse_pos = [0, 0]
     while running:
         for event in pygame.event.get():
@@ -86,17 +81,16 @@ if __name__ == '__main__':
                 prev_mouse_pos = pygame.mouse.get_pos()
             elif event.type == pygame.MOUSEBUTTONUP:
                 drag = False
-
         if drag:
             new_pos = pygame.mouse.get_pos()
             y_angle += (new_pos[0] - prev_mouse_pos[0]) * ROTATE_SPEED
             x_angle += (new_pos[1] - prev_mouse_pos[1]) * ROTATE_SPEED
             prev_mouse_pos = new_pos
-
-        screen.fill(BLACK)
+        glRotatef(0, 0, 0, 0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         for i in range(3):
             for j in range(3):
                 for k in range(3):
                     draw_cube(-1 + i, -1 + j, -1 + k)
-        pygame.display.update()
+        pygame.display.flip()
         pygame.time.Clock().tick(60)
