b93231d437878bd67831549c974d530bd9dadcb2
Damon Holland <damonrholland@gmail.com>
2021-09-21 12:09:50 -0700

    Create categorical encoding for color input for neural network using one-hot encoding

---
+++ ai/CategoricalEncoding.py
@@ -0,0 +1,8 @@
+from model import RubiksCube
+
+EncodingDict = {RubiksCube.RubiksCube.WHITE: "100000",
+             RubiksCube.RubiksCube.GREEN: "010000",
+             RubiksCube.RubiksCube.RED: "001000",
+             RubiksCube.RubiksCube.BLUE: "000100",
+             RubiksCube.RubiksCube.ORANGE: "000010",
+             RubiksCube.RubiksCube.YELLOW: "000001"}
--- ai/RubiksAI.py
+++ ai/RubiksAI.py
@@ -1,12 +1,21 @@
+import ai.CategoricalEncoding as ce
 from threading import Thread
-
 from model.RubiksCube import RubiksCube
 from visuals.RubiksVisualizer import RubiksVisualizer
+
+
+
+def encode_to_input(cube) -> str:
+    encoding = ""
+    for face_color in cube.faces:
+        encoding += ce.EncodingDict[face_color]
+    return encoding
 
 if __name__ == '__main__':
     rubiks_cube = RubiksCube()
     visuals = RubiksVisualizer()
     visual_thread = Thread(target=visuals.start, args=[rubiks_cube])
     visual_thread.start()
+    print("Encoded Input: " + encode_to_input(rubiks_cube))
 
 
