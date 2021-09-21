fde7f6c10b326d3d9fc24ce0c86227ee40dfd2e7
Damon Holland <damonrholland@gmail.com>
2021-09-21 12:24:49 -0700

    Changed encoding to binary rather than one-hot, decreases input layer by half

--- ai/CategoricalEncoding.py
+++ ai/CategoricalEncoding.py
@@ -1,8 +1,8 @@
 from model import RubiksCube
 
-EncodingDict = {RubiksCube.RubiksCube.WHITE: "100000",
-             RubiksCube.RubiksCube.GREEN: "010000",
-             RubiksCube.RubiksCube.RED: "001000",
-             RubiksCube.RubiksCube.BLUE: "000100",
-             RubiksCube.RubiksCube.ORANGE: "000010",
-             RubiksCube.RubiksCube.YELLOW: "000001"}
+EncodingDict = {RubiksCube.RubiksCube.WHITE: "000",
+             RubiksCube.RubiksCube.GREEN: "001",
+             RubiksCube.RubiksCube.RED: "010",
+             RubiksCube.RubiksCube.BLUE: "011",
+             RubiksCube.RubiksCube.ORANGE: "100",
+             RubiksCube.RubiksCube.YELLOW: "101"}
