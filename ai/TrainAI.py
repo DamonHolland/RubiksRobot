f9cce0dbbe9b98fe29b969426d28fe1f01706538
DamonHolland <damonrholland@gmail.com>
2021-10-16 19:03:29 -0700

    TrainAI changes

--- ai/TrainAI.py
+++ ai/TrainAI.py
@@ -1,49 +1,63 @@
-import random
-import time
 import logging
-import ai.RubiksDataset as data
+import os
+import time
+import ai.RubiksDataset as Data
 import tensorflow as tf
 from datetime import timedelta
-from keras.models import Sequential
+from keras import Sequential
 from keras.layers import Dense, Activation
 
 
-def save_model(model, model_name):
-    model.save("models/" + model_name)
+def save_model(model_to_save, model_name):
+    model_to_save.save("models/" + model_name)
+    print("\nModel Saved as {}\n".format(model_name))
+
+
+def load_model(model_name):
+    if model_name:
+        return tf.keras.models.load_model("models/" + model_name)
+    else:
+        return None
 
 
 if __name__ == '__main__':
-    NUM_SCRAMBLES = 5
+    NUM_SCRAMBLES = 3
     LOSS_GOAL = 0.2
     ACCURACY_GOAL = 1
-    MODEL_NAME = "4_0.005543483421206474_1.0"
+    EPOCH_SIZE = 64
+    NUM_EPOCHS = 10
+    EVALUATION_SIZE = 128
+    # Set To None If you want to create a new model
+    # Set to the name of the model if you want to continue training
+    MODEL_NAME = None
 
-    model = tf.keras.models.load_model("models/" + MODEL_NAME)
-
+    model = load_model(MODEL_NAME)
     logging.getLogger('tensorflow').disabled = True
 
-    #model = Sequential()
-    model.add(Dense(162, input_shape=(162,)))
-    model.add(Dense(120))
-    model.add(Activation('relu'))
-    model.add(Dense(12))
-    model.add(Activation('sigmoid'))
+    if not model:
+        model = Sequential()
+        model.add(Dense(162, input_shape=(162,)))
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dense(12))
+        model.add(Activation('sigmoid'))
 
-    model.compile(optimizer="Adam",
-                  loss=["sparse_categorical_crossentropy"],
-                  metrics=["accuracy"])
+        model.compile(optimizer="Adam",
+                      loss=["sparse_categorical_crossentropy"],
+                      metrics=["accuracy"])
 
+    os.system('cls')
     start_time = time.time()
     test_loss, test_acc = 1, 0
     session = 0
     while test_acc < ACCURACY_GOAL or test_loss > LOSS_GOAL:
         session += 1
         print("\nFitting Model Session {}\n".format(session))
-        train_x, train_y = data.create_training_data(100, NUM_SCRAMBLES)
-        model.fit(train_x, train_y, epochs=10)
+        train_x, train_y = Data.create_training_data(EPOCH_SIZE, NUM_SCRAMBLES)
+        model.fit(train_x, train_y, epochs=NUM_EPOCHS, verbose=2)
         print("\nEvaluating Model - Session {}\n".format(session))
-        test_x, test_y = data.create_training_data(100, NUM_SCRAMBLES)
+        test_x, test_y = Data.create_training_data(EVALUATION_SIZE, NUM_SCRAMBLES)
         test_loss, test_acc = model.evaluate(test_x, test_y)
         save_model(model, "Training")
-    save_model(model, str(NUM_SCRAMBLES) + "_" + str(test_loss) + "_" + str(test_acc))
+    save_model(model, str(NUM_SCRAMBLES) + "_" + str(test_acc) + "_" + str(test_loss))
     print("Training Completed in {}".format(timedelta(seconds=time.time() - start_time)))
