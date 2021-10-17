d6d5c88c36b6c2b0dfa37f97910e76bb91c00ad2
DamonHolland <damonrholland@gmail.com>
2021-10-17 14:35:13 -0700

    Dataset generator will check if there are not enough permutations to produce the data

--- ai/RubiksDataset.py
+++ ai/RubiksDataset.py
@@ -14,6 +14,7 @@ def encode_to_input(cube) -> list:
 def create_training_data(data_size, scramble_moves, cube=None):
     training_input = []
     training_output = []
+    fail_count = 0
     new_cube = cube if cube else RubiksCube()
     while len(training_input) < data_size:
         while new_cube.is_solved():
@@ -24,7 +25,14 @@ def create_training_data(data_size, scramble_moves, cube=None):
             if training_input.count(new_input) == 0:
                 training_input.append(new_input)
                 training_output.append(move)
+                fail_count = 0
+            else:
+                fail_count += 1
             perform_move(new_cube, move)
+        # To break if there arent as many permutations as there is requested data
+        if fail_count > data_size:
+            data_size = len(training_input)
+            print("Requested Data too large, not enough permutations")
     return training_input, training_output
 
 
