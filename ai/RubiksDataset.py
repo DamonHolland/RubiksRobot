50a43b620f7b7a5202f4989a114f3983541e783a
Damon Holland <holl9358@eniac.cs.pacificu.edu>
2021-10-18 16:45:26 -0700

    Removed Kociemba Dependecy

--- ai/RubiksDataset.py
+++ ai/RubiksDataset.py
@@ -1,8 +1,32 @@
 import time
 from datetime import timedelta
 from model.RubiksCube import RubiksCube
-from ai.RubiksSolver import solve, perform_move
 
+def perform_move(cube: RubiksCube, move):
+    if move == 0:
+        cube.rotate_white()
+    elif move == 1:
+        cube.rotate_white(True)
+    elif move == 2:
+        cube.rotate_green()
+    elif move == 3:
+        cube.rotate_green(True)
+    elif move == 4:
+        cube.rotate_red()
+    elif move == 5:
+        cube.rotate_red(True)
+    elif move == 6:
+        cube.rotate_blue()
+    elif move == 7:
+        cube.rotate_blue(True)
+    elif move == 8:
+        cube.rotate_orange()
+    elif move == 9:
+        cube.rotate_orange(True)
+    elif move == 10:
+        cube.rotate_yellow()
+    elif move == 11:
+        cube.rotate_yellow(True)
 
 def encode_to_input(cube) -> list:
     encoding = []
@@ -17,9 +41,13 @@ def create_training_data(data_size, scramble_moves, cube=None):
     fail_count = 0
     new_cube = cube if cube else RubiksCube()
     while len(training_input) < data_size:
+        scramble = []
         while new_cube.is_solved():
-            new_cube.scramble(scramble_moves)
-        solve_moves = solve(new_cube)
+            for i in range(scramble_moves):
+                new_cube.scramble(1)
+                scramble.append(new_cube.last_move)
+        scramble.reverse()
+        solve_moves = scramble
         for move in solve_moves:
             new_input = encode_to_input(new_cube)
             if training_input.count(new_input) == 0:
