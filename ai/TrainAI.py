057ff65f885002d96c7519f86ce345e7cdec2347
Damon Holland <damonrholland@gmail.com>
2021-10-19 09:06:48 -0700

    Reset NN structure

--- ai/TrainAI.py
+++ ai/TrainAI.py
@@ -24,10 +24,8 @@ def load_model(model_name):
 
 def create_model():
     new_model = Sequential()
-    new_model.add(Dense(1024, input_shape=(324,), activation='relu'))
-    new_model.add(Dense(2048, input_shape=(1024,), activation='relu'))
-    new_model.add(Dense(1024, input_shape=(2048,), activation='relu'))
-    new_model.add(Dense(12, input_shape=(1024,), activation='softmax'))
+    new_model.add(Dense(228, input_shape=(324,), activation='relu'))
+    new_model.add(Dense(12, input_shape=(228,), activation='softmax'))
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
     new_model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     new_model.summary()
@@ -38,7 +36,7 @@ if __name__ == '__main__':
     NUM_SCRAMBLES = 5
     LOSS_GOAL = 0.2
     ACCURACY_GOAL = 1
-    BATCH_SIZE = 1000
+    BATCH_SIZE = 100000
     NUM_EPOCHS = 10
     EVALUATION_SIZE = 500
     # Set To None If you want to create a new model
