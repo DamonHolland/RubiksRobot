4a43b9b55acbaa5aa357c8817c9d7ed143f0ee88
Damon Holland <damonrholland@gmail.com>
2021-10-19 18:12:17 -0700

    Dataset Uses hashing for faster lookup time to prevent duplicate data generation.

--- ai/RubiksDataset.py
+++ ai/RubiksDataset.py
@@ -1,3 +1,6 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import time
 from datetime import timedelta
 from model.RubiksCube import RubiksCube
@@ -12,31 +15,35 @@ def encode_to_input(cube) -> list:
 
 
 def create_training_data(data_size, scramble_moves):
-    training_input = []
-    training_output = []
+    training_data = dict()
     fail_count = 0
-    while len(training_input) < data_size:
+    while len(training_data.items()) < data_size:
         new_cube = RubiksCube()
         while new_cube.is_solved():
             new_cube.scramble(scramble_moves)
         solve_moves = solve(new_cube)
         for move in solve_moves:
             new_input = encode_to_input(new_cube)
-            if training_input.count(new_input) == 0:
-                training_input.append(new_input)
-                training_output.append(move)
-                perform_move(new_cube, move)
-                fail_count = 0
-            else:
+            try:
+                check_exists = training_data[str(new_input)]
                 fail_count += 1
                 break
+            except KeyError:
+                training_data[str(new_input)] = move
+                perform_move(new_cube, move)
+                fail_count = 0
         # To break if there arent as many permutations as there is requested data
-        # Will Fail if all fails to find new data 10 times in a row
+        # Will fail if all fails to find new data 10 times in a row
         if fail_count > 10:
-            data_size = len(training_input)
-            print("Requested Data too large, not enough permutations. Only {} Found".format(len(training_input)))
-    shuffle = np.random.permutation(len(training_input))
-    return np.array(training_input)[shuffle], np.array(training_output)[shuffle]
+            data_size = len(training_data.items())
+            print("Requested Data too large, not enough permutations. Only {} Found".format(len(training_data.items())))
+    input = []
+    output = []
+    for key, value in training_data.items():
+        input.append(key)
+        output.append(value)
+    shuffle = np.random.permutation(len(input))
+    return np.array(input)[shuffle], np.array(output)[shuffle]
 
 
 if __name__ == '__main__':
