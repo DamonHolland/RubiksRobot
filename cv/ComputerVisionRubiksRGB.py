e152aeb2a4f68bc11a60ae7461bb044c4daae1ce
John Duong <duon3457@pacificu.edu>
2022-02-24 17:36:45 -0800

    Update static, return onehotencoding

--- cv/ComputerVisionRubiksRGB.py
+++ cv/ComputerVisionRubiksRGB.py
@@ -9,31 +9,32 @@ class RGBUint8:
         pixel_center = hsv_frame[y, x]
         hue_value = pixel_center[0]
 
-        color = "Undefined"
+        color = None
 
         if hue_value < 5:
-            #RED
+            # Red
             color = (0, 0, 0, 1, 0, 0)
         elif hue_value < 22:
-            #ORANGE
+            # Orange
             color = (0, 1, 0, 0, 0, 0)
         elif hue_value < 33:
-            #YELLOW
+            # Yellow
             color = (1, 0, 0, 0, 0, 0)
         elif hue_value < 78:
-            #GREEN
+            # Green
             color = (0, 0, 0, 0, 1, 0)
         elif hue_value < 131:
-            #BLUE
+            # Blue
             color = (0, 0, 1, 0, 0, 0)
         else:
-            #RED
+            # Red
             color = (0, 0, 0, 1, 0, 0)
 
         pixel_center_bgr = frame[y, x]
         b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
 
         if b > 150 and g > 150 and r > 150:
+            # White
             color = (0, 0, 0, 0, 0, 1)
 
         return color
